fca9729619fb03be475f9ab7b9d2458c1612f95a
Neto test <sysl86@mail.ru>
2023-07-11 13:22:01 +0400

    Intiall commit

---
+++ src/main/java/runetology/MovieManager.java
@@ -0,0 +1,20 @@
+package runetology;
+public class MovieManager {
+    private String [] movies = new String[0];
+
+
+
+    public void addMovie(String movie){
+
+
+    }
+    public String[] findAll(){
+
+
+    }
+  //  public String[] findLast(){
+
+
+    }
+
+}
